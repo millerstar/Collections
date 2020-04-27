225afa154d84d2001c4d55959b83e7cb33995cda
Ilan Miller <millerstar@gmail.com>
2020-04-28 00:11:30 +0300

    Assignment 2: Words in Files

---
+++ src/main/java/com/coursera/randomstory/WordsInFiles.java
@@ -0,0 +1,76 @@
+package com.coursera.randomstory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class WordsInFiles {
+    public static final String FILE_1 = "src/main/resources/brief1.txt";
+    public static final String FILE_2 = "src/main/resources/brief2.txt";
+    public static final String FILE_3 = "src/main/resources/brief3.txt";
+    public static final String FILES_SOURCE_DIR = "src/main/resources/briefs";
+    private HashMap<String, ArrayList<String>> wordsInFilesMap;
+
+    public WordsInFiles() {
+        wordsInFilesMap = new HashMap<>();
+    }
+
+    private void addWordsFromFile(File file) {
+        try {
+            File myFile = new File(String.valueOf(file));
+            Scanner scanner = new Scanner(myFile);
+            while (scanner.hasNext() == true) {
+                String word = scanner.next();
+                if (!wordsInFilesMap.containsKey(word)) {
+                    ArrayList<String> fileNameList = new ArrayList<>();
+                    fileNameList.add(file.getName());
+                    wordsInFilesMap.put(word, fileNameList);
+                } else {
+                    if (!wordsInFilesMap.get(word).contains(file.getName())) {
+                        wordsInFilesMap.get(word).add(file.getName());
+                    }
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("file was not found!");
+        }
+    }
+
+    public void buildWordFileMap() {
+        wordsInFilesMap.clear();
+        ArrayList<String> filesList = getFilesFromFolderList(new File(FILES_SOURCE_DIR));
+        for (String filePath : filesList) {
+            addWordsFromFile(new File(filePath));
+        }
+        System.out.println("MAx value is: " + maxNumber());
+    }
+
+    private ArrayList<String> getFilesFromFolderList(final File folder) {
+        ArrayList<String> fileEntryList = new ArrayList<>();
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                getFilesFromFolderList(fileEntry);
+            } else {
+                fileEntryList.add(fileEntry.getPath());
+            }
+        }
+        return fileEntryList;
+    }
+
+    private int maxNumber() {
+        int maxVal = 0;
+        for (ArrayList<String> files : wordsInFilesMap.values()) {
+            if (maxVal < files.size()) {
+                maxVal = files.size();
+            }
+        }
+        return maxVal;
+    }
+
+    public static void main(String[] args) {
+        WordsInFiles wordsInFiles = new WordsInFiles();
+        wordsInFiles.buildWordFileMap();
+    }
+}
