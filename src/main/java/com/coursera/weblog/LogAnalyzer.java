cc63930970a441a648364a377d5218ebc8125dec
Ilan Miller <millerstar@gmail.com>
2020-05-07 23:35:47 +0300

    Assignment: Reading Web Logs

--- src/main/java/com/coursera/weblog/LogAnalyzer.java
+++ src/main/java/com/coursera/weblog/LogAnalyzer.java
@@ -1,31 +1,38 @@
 package com.coursera.weblog;
 /**
  * Write a description of class LogAnalyzer here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 
 import java.util.*;
+
 import com.coursera.edu.duke.*;
 
-public class LogAnalyzer
-{
-     private ArrayList<LogEntry> records;
-     
-     public LogAnalyzer() {
-         // complete constructor
-     }
-        
-     public void readFile(String filename) {
-         // complete method
-     }
-        
-     public void printAll() {
-         for (LogEntry le : records) {
-             System.out.println(le);
-         }
-     }
-     
-     
+public class LogAnalyzer {
+    private ArrayList<LogEntry> records;
+
+    public LogAnalyzer() {
+        // complete constructor
+        records = new ArrayList<>();
+        records.clear();
+    }
+
+    public void readFile(String filename) {
+        // complete method
+        FileResource fileResource = new FileResource(filename);
+        for (String line : fileResource.lines()) {
+            LogEntry logEntry  = WebLogParser.parseEntry(line);
+            records.add(logEntry);
+        }
+    }
+
+    public void printAll() {
+        for (LogEntry le : records) {
+            System.out.println(le);
+        }
+    }
+
+
 }
--- src/main/java/com/coursera/weblog/Tester.java
+++ src/main/java/com/coursera/weblog/Tester.java
@@ -10,6 +10,7 @@ import java.util.*;
 
 public class Tester
 {
+    public static final String FILE_PATH = "src/main/resources/short-test_log.txt";
     public void testLogEntry() {
         LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
         System.out.println(le);
@@ -19,5 +20,13 @@ public class Tester
     
     public void testLogAnalyzer() {
         // complete method
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.readFile(FILE_PATH);
+        logAnalyzer.printAll();
+    }
+
+    public static void main(String[] args) {
+        Tester tester = new Tester();
+        tester.testLogAnalyzer();
     }
 }
